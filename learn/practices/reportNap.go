aeea106f9453b7668b6ec136145a0609f1da0c59
ramseyjiang <davy820ramsey@gmail.com>
2021-11-21 16:45:34 +1300

    update comment for goroutine practice

--- learn/practices/reportNap.go
+++ learn/practices/reportNap.go
@@ -24,12 +24,17 @@ func send(reportChannel chan string) {
 /**
 Every second the goroutine is asleep, it will print an announcement that it’s still sleeping.
 
+It starts with a reportNap function that causes the current goroutine to sleep for a specified number of seconds.
+Every second the goroutine is asleep, it will print an announcement that it’s still sleeping.
+
 It calls a send function that will run in a goroutine and send two values to a channel.
 Before it sends anything, though, it first calls reportNap so its goroutine sleeps for 2 seconds.
 
-When we run this, we’ll see both goroutines sleep for the first 2 seconds. Then the send goroutine wakes up and
+When it runs, it will make both goroutines sleep for the first 2 seconds. Then the send goroutine wakes up and
 sends its value. But it does not do anything further; the send operation blocks the send goroutine until the
-main goroutine receives the value.
+main goroutine receives the value. That does not happen right away, because the main goroutine still needs to
+sleep for 3 more seconds. When it wakes up, it receives the value from the channel. Only then is the send goroutine
+unblocked. After that, it can send its second value.
  */
 func main() {
 	reportChannel := make(chan string)
@@ -40,9 +45,14 @@ func main() {
 }
 /**
 go run practices/reportNap.go, Result:
+From 54 to 58, sending and receiving goroutine are asleep.
+From 59 to 60, the sending goroutine wakes yp and sends a value.
+From 61 to 62, the receiving goroutine is still sleeping.
+From 63 to 64, the receiving goroutine wakes up and receives a value.
+From 65 to 66, only then, the sending goroutine is unblocked, so it can send its second value.
 
-Receiving goroutine 0 sleeping	//it will invoke reportNap(), then it will run fmt.Println() in it, then sleep
-sending goroutine 0 sleeping	//
+Receiving goroutine 0 sleeping
+sending goroutine 0 sleeping
 sending goroutine 1 sleeping
 Receiving goroutine 1 sleeping
 Receiving goroutine 2 sleeping
