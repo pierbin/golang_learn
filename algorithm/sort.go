34dde495b8158e14ebff8262d044f885b6327dce
ramseyjiang <davy820ramsey@gmail.com>
2021-12-04 22:57:13 +1300

    add redis usage

--- algorithm/sort.go
+++ algorithm/sort.go
@@ -14,6 +14,7 @@ func main() {
 	fmt.Println("Quick sorted array is: ", QuickSort(arr))
 	fmt.Println("Merge sorted array is: ", MergeSort(arr))
 	fmt.Println("Selection sorted array is: ", SelectionQuick(arr))
+	fmt.Println("Insertion sorted array is: ", insertionSort(arr))
 }
 
 func generateUnsortedArr(n int, numMax int) []int{
@@ -25,6 +26,7 @@ func generateUnsortedArr(n int, numMax int) []int{
 	return arr
 }
 
+//Time complexity is O(n*n)
 func BubbleSort(arr []int) []int {
 	for i := 0; i < len(arr); i++ {
 		for j := len(arr) - 1; j > i; j-- {
@@ -37,6 +39,7 @@ func BubbleSort(arr []int) []int {
 	return arr
 }
 
+//Time complexity is O(nlogn), the best is O(nlogn), the worst is O(n*n)
 func QuickSort(arr []int) []int {
 	if len(arr) <= 1 {
 		return arr
@@ -76,6 +79,7 @@ func MergeSort(arr []int) []int {
 	return Merge(left, right)
 }
 
+//Time complexity is O(nlogn), the best and the worst are the same, it is O(nlogn)
 func Merge(left, right []int) (result []int) {
 	lIndex, rIndex := 0, 0
 	for lIndex < len(left) && rIndex < len(right) {
@@ -93,6 +97,7 @@ func Merge(left, right []int) (result []int) {
 	return result
 }
 
+//Time complexity is O(n*n), the best is O(n*n), the worst is O(n*n)
 func SelectionQuick(arr []int) []int {
 	if len(arr) <= 1 {
 		return arr
@@ -114,5 +119,15 @@ func SelectionQuick(arr []int) []int {
 		}
 	}
 
+	return arr
+}
+
+//Time complexity is O(n*n), the best is O(n), the worst is O(n*n)
+func insertionSort(arr []int) []int {
+	for i := 0; i < len(arr); i++ {
+		for j := i; j > 0 && arr[j-1] > arr[j]; j-- {
+			arr[j], arr[j-1] = arr[j-1], arr[j]
+		}
+	}
 	return arr
 }
