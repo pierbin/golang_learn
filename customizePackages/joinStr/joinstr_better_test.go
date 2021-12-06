e0a1c01ba59c615cf7bfffa5e6032c34cffbdb38
ramseyjiang <davy820ramsey@gmail.com>
2021-12-06 15:31:41 +1300

    adjust directory structure update

---
+++ customizePackages/joinStr/joinstr_better_test.go
@@ -0,0 +1,59 @@
+package joinStr
+
+import (
+	"fmt"
+	"testing"
+)
+
+//If the tests in the same package with codes, please run tests "click run button."
+//If not, you can run tests using any of the following ways.
+
+//This test is better than joinstr_usual_test.go, because it moves common check parts in three different tests outside
+//in a same function. Then it will use less time to run all tests anf faster than joinstr_usual_test.go.
+//The compare result is as following:
+/**
+% go test base/tests/joinstr_usual_test.go
+ok      command-line-arguments  0.007s
+% go test base/tests/joinstr_better_test.go
+ok      command-line-arguments  0.006s
+*/
+
+//The way in this file it is called Table-driven tests.
+//Table-driven tests are tests that process “tables” of inputs and expected outputs. They pass each set of input to
+//the code being tested, and check that the code’s output matches the expected values.
+
+/**	The upgrade
+ go test base/tests/joinstr_better_test.go -v
+=== RUN   TestJoinStr
+    joinstr_better_test.go:41: Success, JoinStr([]string(nil)), want "", got "", it passed
+    joinstr_better_test.go:41: Success, JoinStr([]string{"apple"}), want "apple", got "apple", it passed
+    joinstr_better_test.go:41: Success, JoinStr([]string{"apple", "orange"}), want "apple and orange", got "apple and orange", it passed
+    joinstr_better_test.go:41: Success, JoinStr([]string{"apple", "orange", "pear"}), want "apple, orange, and pear", got "apple, orange, and pear", it passed
+--- PASS: TestJoinStr (0.00s)
+PASS
+ok      command-line-arguments  0.011s
+
+*/
+
+type testData struct {
+	list []string //The slice we'll pass to target method.
+	want string   //The string we expect return.
+}
+
+func TestJoinStr(t *testing.T) {
+	tests := []testData{
+		{},
+		{list: []string{"apple"}, want: "apple"},
+		{list: []string{"apple", "orange"}, want: "apple and orange"},
+		{list: []string{"apple", "orange", "pear"}, want: "apple, orange, and pear"},
+	}
+
+	for _, test := range tests {
+		got := JoinWithCommas(test.list)
+		if got != test.want {
+			t.Errorf(fmt.Sprintf("JoinWithCommas(%#v) = \"%s\", want \"%s\"", test.list, got, test.want))
+		} else {
+			t.Logf(fmt.Sprintf("Success, JoinStr(%#v), want \"%s\", got \"%s\", it passed", test.list, test.want, got))
+		}
+	}
+}
